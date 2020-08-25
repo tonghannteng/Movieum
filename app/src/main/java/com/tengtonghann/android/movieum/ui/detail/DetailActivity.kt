f0d73481638f4e9369b3dd51b7ef5a9ac432a103
Tonghann Teng <tengtonghann@gmail.com>
2020-08-24 23:36:12 -0500

    Add detail screen

---
+++ app/src/main/java/com/tengtonghann/android/movieum/ui/detail/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.tengtonghann.android.movieum.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
