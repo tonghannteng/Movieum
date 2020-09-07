dade8a0ffbcb30848cea2a8ad444180f184d9fbd
Tonghann Teng <tengtonghann@gmail.com>
2020-09-07 15:39:43 -0500

    check no connection in the first screen

--- app/src/main/java/com/tengtonghann/android/movieum/ui/main/MainActivity.kt
+++ app/src/main/java/com/tengtonghann/android/movieum/ui/main/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.tengtonghann.android.movieum.ui.main
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.view.View
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -8,6 +11,8 @@ import com.tengtonghann.android.movieum.databinding.ActivityMainBinding
 import com.tengtonghann.android.movieum.ui.base.BaseActivity
 import com.tengtonghann.android.movieum.ui.favorite.FavoriteFragment
 import com.tengtonghann.android.movieum.ui.movie.MovieFragment
+import com.tengtonghann.android.movieum.utils.NetworkUtils
+import com.tengtonghann.android.movieum.utils.getColorRes
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -17,6 +22,7 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     companion object {
         const val TAG = "MainActivity"
+        const val ANIMATION_DURATION = 1000.toLong()
     }
 
     /**
@@ -98,6 +104,44 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
         mViewModel.favoriteNavigationLiveData.observe(this, Observer {
             it.getIfNotHandled()?.run { showFavorite() }
         })
+
+        handleNetworkChanges()
+    }
+
+    /**
+     * Observe network changes: Internet Connectivity
+     */
+    private fun handleNetworkChanges() {
+        NetworkUtils.getNetworkLiveData(applicationContext).observe(
+            this,
+            Observer { isConnected ->
+                if (!isConnected) {
+                    mViewBinding.textViewNetworkStatus.text =
+                        getString(R.string.text_no_connectivity)
+                    mViewBinding.networkStatusLayout.apply {
+                        visibility = View.VISIBLE // show
+                        setBackgroundColor(getColorRes(R.color.colorStatusNotConnected))
+                    }
+                } else {
+                    mViewBinding.textViewNetworkStatus.text =
+                        getString(R.string.text_connectivity)
+                    mViewBinding.networkStatusLayout.apply {
+                        setBackgroundColor(getColorRes(R.color.colorStatusConnected))
+
+                        // TODO: fixed animation
+                        animate()
+                            .alpha(1f)
+                            .setStartDelay(ANIMATION_DURATION)
+                            .setDuration(ANIMATION_DURATION)
+                            .setListener(object : AnimatorListenerAdapter() {
+                                override fun onAnimationEnd(animation: Animator) {
+                                    visibility = View.GONE
+                                }
+                            })
+                    }
+                }
+            }
+        )
     }
 
 }
